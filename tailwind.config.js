c78f34d541072f39b27705c0f23a70a0f33cbcd2
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-04-23 23:27:08 -0400

    Tailwind CSS Theme colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                custom: {
+                    bg: "#2D3934",
+                    ly1: "#4F675E",
+                    ly2: "#394942",
+                    secondary: "#69AA95",
+                    primary: "#A0CFC0",
+                    accent: "#DACEAF",
+                },
+            },
+        },
     },
     plugins: [],
 };
