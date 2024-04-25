7c9d2ceba18e4da619da10525ef2f672ae893b85
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-04-25 00:14:45 -0400

    Section state changing config

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changeSection } from "../reducers/sectionReducer";
 
 export default function Sidebar() {
+    interface sectionRoute {
+        section: string;
+        route: string;
+    }
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleNavigation = (section: sectionRoute) => {
+        dispatch(changeSection(section.section));
+        navigate(section.route);
+    };
+
     return (
         <div className="bg-custom-ly1 w-52">
             <h2>AppName</h2>
             <div className="flex flex-col">
-                <Link to={"/"}>Dashboard</Link>
-                <Link to={"/budget"}>Budget</Link>
-                <Link to={"/expenses"}>Expenses</Link>
-                <Link to={"/Savings"}>Savings</Link>
+                <button onClick={() => handleNavigation({ section: "Dashboard", route: "/" })}>
+                    Dashboard
+                </button>
+                <button onClick={() => handleNavigation({ section: "Budget", route: "/budget" })}>
+                    Budget
+                </button>
+                <button onClick={() => handleNavigation({ section: "Expenses", route: "/expenses" })}>
+                    Expenses
+                </button>
+                <button onClick={() => handleNavigation({ section: "Savings", route: "/savings" })}>
+                    Savings
+                </button>
             </div>
         </div>
     );
