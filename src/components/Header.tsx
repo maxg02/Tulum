007ac70c6d8b090767e5436be11ebbfa30a489e4
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-04-27 00:46:15 -0400

    Font style defining

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ import { SectionType } from "./Sidebar";
 export default function Header({ currentSection }: { currentSection: SectionType }) {
     return (
         <div className="flex items-center justify-between h-14">
-            <h1 className="font-title">{currentSection}</h1>
+            <h1>{currentSection}</h1>
             <div className="h-full flex items-center gap-5">
-                <img className="h-full w-auto rounded-xl" src="/profilePic.jpeg" />
-                <h4 className="font-body">Maria Ambroise</h4>
+                <img className="h-full w-auto rounded-xl" src="/profilePic.jpeg" alt="Profile Pic" />
+                <h4>Maria Ambroise</h4>
             </div>
         </div>
     );
