2d7d60dcccecc09c04828afa9937396a5d3ef45a
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-04-26 22:25:14 -0400

    Deleting Section Reducer

--- src/store.tsx
+++ src/store.tsx
@@ -1,8 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sectionReducer from "./reducers/sectionReducer";
 
 export const store = configureStore({
-    reducer: {
-        section: sectionReducer,
-    },
+    reducer: {},
 });
