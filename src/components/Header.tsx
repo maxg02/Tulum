a566e9483a055673817e2ffcaffcb783f9edaba1
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-04-26 22:57:48 -0400

    Header designing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,5 +2,13 @@ import React from "react";
 import { SectionType } from "./Sidebar";
 
 export default function Header({ currentSection }: { currentSection: SectionType }) {
-    return <div className="bg-green-400">{currentSection}</div>;
+    return (
+        <div className="flex items-center justify-between h-14">
+            <h1 className="font-title">{currentSection}</h1>
+            <div className="h-full flex items-center gap-5">
+                <img className="h-full w-auto rounded-xl" src="/profilePic.jpeg" />
+                <h4 className="font-body">Maria Ambroise</h4>
+            </div>
+        </div>
+    );
 }
--- src/components/SectionContent.tsx
+++ src/components/SectionContent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { ReactElement } from "react";
 
 function SectionContent({ children }: { children: ReactElement[] }) {
-    return <div className="flex-1 flex flex-col py-8">{children}</div>;
+    return <div className="flex-1 flex flex-col py-8 gap-4">{children}</div>;
 }
 
 export default SectionContent;
