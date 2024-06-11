2e3e00bddc8de7f3d040719977aecc063ebfbe53
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-06-11 16:17:21 -0400

    Colors enums creation

---
+++ src/components/Colors.tsx
@@ -0,0 +1,16 @@
+export enum basicColors {
+    bg = "#2D3934",
+    ly1 = "#4F675E",
+    ly2 = "#394942",
+    secondary = "#69AA95",
+    primary = "#A0CFC0",
+    accent = "#DACEAF",
+}
+
+export enum gradientColors {
+    "#D2EBE5",
+    "#99CCBC",
+    "#56A98F",
+    "#336656",
+    "#24433A",
+}
