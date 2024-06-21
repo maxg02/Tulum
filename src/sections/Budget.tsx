864f6154542f42d96d58146dd3af460d87c1d827
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-06-21 00:48:37 -0400

    Budget section layout

--- src/sections/Budget.tsx
+++ src/sections/Budget.tsx
@@ -9,7 +9,13 @@ export default function Budget() {
             <Sidebar currentSection="Budget" />
             <SectionContent>
                 <Header currentSection="Budget" />
-                <div className="bg-purple-400 flex-1">Budget</div>
+                <div className="flex-1 grid grid-cols-2 grid-rows-12 grid-flow-col overflow-hidden gap-9">
+                    <div className="row-span-3 infoContainer1"></div>
+                    <div className="row-span-3 infoContainer1"></div>
+                    <div className="row-span-6 infoContainer1"></div>
+                    <div className="row-span-7 infoContainer2"></div>
+                    <div className="row-span-5 infoContainer2"></div>
+                </div>
             </SectionContent>
         </div>
     );
