fc3db5c24fa813be0489b971c8e615b801d55a1f
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-06-21 00:48:53 -0400

    Expenses section layout

--- src/sections/Expenses.tsx
+++ src/sections/Expenses.tsx
@@ -9,7 +9,12 @@ export default function Expenses() {
             <Sidebar currentSection="Expenses" />
             <SectionContent>
                 <Header currentSection="Expenses" />
-                <div className="bg-purple-400 flex-1">Expenses</div>
+                <div className="flex-1 grid grid-cols-14 grid-rows-12 grid-flow-col overflow-hidden gap-9">
+                    <div className="row-span-6 col-span-6 infoContainer1"></div>
+                    <div className="row-span-6 col-span-7 infoContainer2"></div>
+                    <div className="row-span-6 col-span-8 infoContainer1"></div>
+                    <div className="row-span-6 col-span-7 infoContainer2"></div>
+                </div>
             </SectionContent>
         </div>
     );
