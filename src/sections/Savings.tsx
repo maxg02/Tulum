8f9e7d88f57dc18f151258121ef52578ec4f005a
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-06-21 00:49:05 -0400

    Savings section layout

--- src/sections/Savings.tsx
+++ src/sections/Savings.tsx
@@ -9,7 +9,12 @@ export default function Savings() {
             <Sidebar currentSection="Savings" />
             <SectionContent>
                 <Header currentSection="Savings" />
-                <div className="bg-purple-400 flex-1">Savings</div>
+                <div className="flex-1 grid grid-cols-16 grid-rows-12 grid-flow-row overflow-hidden gap-9">
+                    <div className="row-span-12 col-span-3 infoContainer1"></div>
+                    <div className="row-start-1 row-span-6 col-start-4 col-span-6 infoContainer1"></div>
+                    <div className="row-start-1 row-span-6 col-span-7 infoContainer2"></div>
+                    <div className="row-start-7 row-span-6 col-start-4 col-[13_/_span_13] infoContainer2"></div>
+                </div>
             </SectionContent>
         </div>
     );
