fbb858eba5a20a51b0e978f2c488a491424874cd
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-06-24 06:40:40 -0400

    Sections structure

--- src/sections/Budget.tsx
+++ src/sections/Budget.tsx
@@ -10,7 +10,10 @@ export default function Budget() {
             <SectionContent>
                 <Header currentSection="Budget" />
                 <div className="flex-1 grid grid-cols-2 grid-rows-12 grid-flow-col overflow-hidden gap-9">
-                    <div className="row-span-3 infoContainer1"></div>
+                    <div className="row-span-3 infoContainer1 justify-items-center">
+                        <p>Total Budget</p>
+                        <h1 className="font-light text-5xl flex-1 flex items-center">RD$75000</h1>
+                    </div>
                     <div className="row-span-3 infoContainer1"></div>
                     <div className="row-span-6 infoContainer1"></div>
                     <div className="row-span-7 infoContainer2"></div>
