ef2b8759887b58c463f522cfdbd7cc0602c08782
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-07-08 16:52:42 -0400

    Budget fixed income table and flex layout

--- src/sections/Budget.tsx
+++ src/sections/Budget.tsx
@@ -21,7 +21,6 @@ export default function Budget() {
             ["Transport", 4000, 0],
             ["House/Utilities", 3000, 2],
             ["Personal/Medical", 3250, 1],
-            ["University", 3800, 3],
         ],
     };
 
@@ -29,7 +28,7 @@ export default function Budget() {
         columns: [
             { name: "Income", type: "amount" },
             { name: "Details", type: "string" },
-            { name: "Date", type: "string" },
+            { name: "Date", type: "date" },
         ],
         rows: [
             [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", "06/05/24"],
@@ -37,6 +36,26 @@ export default function Budget() {
             [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", "12/04/24"],
             [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", "05/04/24"],
             [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", "25/03/24"],
+            [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", "05/04/24"],
+            [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", "15/04/24"],
+        ],
+    };
+
+    const fixedIncomeData: dataObject = {
+        columns: [
+            { name: "Income", type: "amount" },
+            { name: "Details", type: "string" },
+            {
+                name: "Periodicity",
+                type: "list",
+                values: ["Annual", "Monthly", "Biweekly", "Weekly"],
+            },
+        ],
+        rows: [
+            [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", 0],
+            [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", 1],
+            [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", 2],
+            [3500, "Lorem ipsum dolor sit amet consectetur. Mauris fusce.", 3],
         ],
     };
 
@@ -45,34 +64,43 @@ export default function Budget() {
             <Sidebar currentSection="Budget" />
             <SectionContent>
                 <Header currentSection="Budget" />
-                <div className="flex-1 grid grid-cols-2 grid-rows-12 grid-flow-col overflow-hidden gap-9">
-                    <div className="row-span-3 infoContainer1 justify-items-center">
-                        <p>Total Budget</p>
-                        <h1 className="font-light text-5xl flex-1 flex items-center">RD$75000</h1>
-                    </div>
-                    <div className="row-span-3  flex gap-x-9">
-                        <div className="infoContainer1 flex-1 bg-gradient-to-b from-custom-secondary to-custom-accent shadow-none">
-                            <p>January Budget</p>
-                            <h1 className="font-light text-5xl flex-1 flex items-center">RD$75000</h1>
+                <div className="flex-1 flex overflow-hidden gap-x-9">
+                    <div className="flex flex-col w-5/12 gap-y-9">
+                        <div className="infoContainer1 h-[18%]">
+                            <p>Total Budget</p>
+                            <h1 className="font-light text-5xl my-auto">RD$75000</h1>
                         </div>
-                        <div className="infoContainer1 flex-1 bg-gradient-to-b from-custom-secondary to-custom-accent shadow-none">
-                            <p>2024 Budget</p>
-                            <h1 className="font-light text-5xl flex-1 flex items-center">RD$750K</h1>
+                        <div className="flex gap-x-9 h-[18%]">
+                            <div className="infoContainer1 flex-1 bg-gradient-to-b from-custom-secondary to-custom-accent shadow-none">
+                                <p>January Budget</p>
+                                <h1 className="font-light text-5xl my-auto">RD$75000</h1>
+                            </div>
+                            <div className="infoContainer1 flex-1 bg-gradient-to-b from-custom-secondary to-custom-accent shadow-none">
+                                <p>2024 Budget</p>
+                                <h1 className="font-light text-5xl my-auto">RD$750K</h1>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row-span-6 infoContainer1">
-                        <p>Budget Planning</p>
-                        <div className="flex items-center flex-1 w-full">
-                            <Table data={budgetPlanningData} />
-                        </div>
-                    </div>
-                    <div className="row-span-7 infoContainer2">
-                        <p>Income</p>
-                        <div className="flex items-center flex-1 w-full">
-                            <Table dark data={incomeData} />
+                        <div className="infoContainer1 flex-1">
+                            <p>Budget Planning</p>
+                            <div className="flex flex-1 items-center w-full">
+                                <Table data={budgetPlanningData} tablePrefix="BP" />
+                            </div>
                         </div>
                     </div>
-                    <div className="row-span-5 infoContainer2"></div>
+                    <div className="flex flex-col flex-1 gap-y-9">
+                        <div className="flex-1 infoContainer2">
+                            <p>Income</p>
+                            <div className="flex items-center flex-1 w-full">
+                                <Table dark data={incomeData} tablePrefix="I" />
+                            </div>
+                        </div>
+                        <div className="infoContainer2">
+                            <p>Fixed Income</p>
+                            <div className="flex items-center w-full">
+                                <Table data={fixedIncomeData} tablePrefix="FI" dark />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </SectionContent>
         </div>
