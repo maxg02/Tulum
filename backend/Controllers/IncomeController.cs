6c04982af284eefb93b9474c199f5d77797f811c
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-08-19 23:52:04 -0400

    Creation of Income Get and Post Controllers

---
+++ backend/Controllers/IncomeController.cs
@@ -0,0 +1,48 @@
+﻿using backend.Data;
+using backend.Dtos.Income;
+using backend.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncomeController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public IncomeController(ApplicationDBContext dBContext) => _context = dBContext;
+
+        [HttpGet("user/{userId}")]
+        public IActionResult GetUserIncome([FromRoute] int userId)
+        {
+            var incomes = _context.Incomes
+                .Where(i => i.UserId==userId)
+                .ToList()
+                .Select(s => s.ToIncomeDto());
+                
+            return Ok(incomes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetIncome([FromRoute] int id)
+        {
+            var income = _context.Incomes.FirstOrDefault(i => i.Id==id);
+
+            if (income == null) {
+                return NotFound();
+            }
+
+            return Ok(income.ToIncomeDto());
+        }
+        [HttpPost]
+        public IActionResult CreateIncome([FromBody] CreateIncomeRequestDto incomeDto)
+        {
+            var incomeModel = incomeDto.ToIncomeFromCreateDto();
+            _context.Incomes.Add(incomeModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetIncome), new { id = incomeModel.Id }, incomeModel.ToIncomeDto());
+        }
+
+    }
+}
