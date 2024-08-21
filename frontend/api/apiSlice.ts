51e165efceb640650efdb26eae743285fe2357d4
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-08-21 16:50:01 -0400

    ApiSlice baseUrl and query endpoint fixing

--- frontend/api/apiSlice.ts
+++ frontend/api/apiSlice.ts
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "localhost:5058/api/",
+        baseUrl: "http://localhost:5085/api/",
     }),
     endpoints: (builder) => ({
         getUserIncome: builder.query({
-            query: (userId) => `user/${userId}`,
+            query: (userId) => `income/user/${userId}`,
         }),
     }),
 });
