8981b58adc388c4e7e939608c3ec0dc841fe7898
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-08-25 22:48:37 -0400

    Fixed Income mapper created

---
+++ backend/Mappers/FixedIncomeMappers.cs
@@ -0,0 +1,33 @@
+﻿using backend.Dtos.FixedIncome;
+using backend.Models;
+using backend.Enums;
+
+namespace backend.Mappers
+{
+    public static class FixedIncomeMappers
+    {
+        public static FixedIncomeDto ToFixedIncomeDto(this FixedIncome FixedIncomeModel)
+        {
+            Periodicity periodicityValue = (Periodicity)FixedIncomeModel.Periodicity;
+            
+            return new FixedIncomeDto
+            {
+                Id = FixedIncomeModel.Id,
+                Amount = FixedIncomeModel.Amount,
+                Detail = FixedIncomeModel.Detail,
+                Periodicity = periodicityValue.ToString()
+            };
+        }
+
+        public static FixedIncome ToFixedIncomeFromCreateDto(this CreateFixedIncomeRequestDto FixedIncomeDto)
+        {
+            return new FixedIncome
+            {
+                Amount = FixedIncomeDto.Amount,
+                Detail = FixedIncomeDto.Detail,
+                Periodicity = FixedIncomeDto.Periodicity,
+                UserId = 1
+            };
+        }
+    }
+}
