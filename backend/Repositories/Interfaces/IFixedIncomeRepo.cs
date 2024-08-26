e9971317261bf12aa884c36c93ee26f91fd17e15
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-08-25 22:47:50 -0400

    Fixed Income Repo interface Created

---
+++ backend/Repositories/Interfaces/IFixedIncomeRepo.cs
@@ -0,0 +1,14 @@
+﻿using backend.Dtos.FixedIncome;
+using backend.Models;
+
+namespace backend.Repositories.Interfaces
+{
+    public interface IFixedIncomeRepo
+    {
+        Task<List<FixedIncome>> GetByUserIdAsync(int userId);
+        Task<FixedIncome?> GetByIdAsync(int Id);
+        Task<FixedIncome> CreateAsync(FixedIncome fixedIncome);
+        Task<FixedIncome?> UpdateAsync(int Id, UpdateFixedIncomeRequestDto fixedIncomeDto);
+        Task<FixedIncome?> DeleteAsync(int Id);
+    }
+}
