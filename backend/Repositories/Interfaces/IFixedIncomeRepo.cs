a49c9b99ed83917c07ce738fc0cd66d5ce4d7deb
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-08-27 11:18:21 -0400

    Id -> id

--- backend/Repositories/Interfaces/IFixedIncomeRepo.cs
+++ backend/Repositories/Interfaces/IFixedIncomeRepo.cs
@@ -6,9 +6,9 @@ namespace backend.Repositories.Interfaces
     public interface IFixedIncomeRepo
     {
         Task<List<FixedIncome>> GetByUserIdAsync(int userId);
-        Task<FixedIncome?> GetByIdAsync(int Id);
+        Task<FixedIncome?> GetByIdAsync(int id);
         Task<FixedIncome> CreateAsync(FixedIncome fixedIncome);
-        Task<FixedIncome?> UpdateAsync(int Id, UpdateFixedIncomeRequestDto fixedIncomeDto);
-        Task<FixedIncome?> DeleteAsync(int Id);
+        Task<FixedIncome?> UpdateAsync(int id, UpdateFixedIncomeRequestDto fixedIncomeDto);
+        Task<FixedIncome?> DeleteAsync(int id);
     }
 }
