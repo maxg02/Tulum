91fd1f879cc35837c0c3117930eaf1f673741b1b
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-08-27 11:18:39 -0400

    ExpenseCategory Interface creation

---
+++ backend/Repositories/Interfaces/IExpenseCategoryRepo.cs
@@ -0,0 +1,14 @@
+﻿using backend.Dtos.ExpenseCategory;
+using backend.Models;
+
+namespace backend.Repositories.Interfaces
+{
+    public interface IExpenseCategoryRepo
+    {
+        Task<List<ExpenseCategory>> GetByUserIdAsync(int userId);
+        Task<ExpenseCategory?> GetByIdAsync(int id);
+        Task<ExpenseCategory> CreateAsync(ExpenseCategory expenseCategory);
+        Task<ExpenseCategory?> UpdateAsync(int id, UpdateExpenseCategoryRequestDto expenseCategoryDto);
+        Task<ExpenseCategory?> DeleteAsync(int id);
+    }
+}
