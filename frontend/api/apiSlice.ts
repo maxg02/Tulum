5bdd73d2b3e4089e4bc0eba80e75a3eb0413faf3
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-13 15:18:35 -0400

    Fixed income endpoints configuration and tag invalidation implementation

--- frontend/api/apiSlice.ts
+++ frontend/api/apiSlice.ts
@@ -3,13 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export type createIncomeDto = { amount: number; details: string; date: Date };
 export type updateIncomeDto = { id: number; data: { amount: number; details: string; date: Date } };
 
+export type createFixedIncomeDto = { amount: number; details: string; periodicity: number };
+
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:5085/api/",
     }),
+    tagTypes: ["Income", "FixedIncome"],
     endpoints: (builder) => ({
         getIncomesByUserId: builder.query({
             query: (userId: number) => `income/user/${userId}`,
+            providesTags: ["Income"],
         }),
         getIncomesById: builder.query({
             query: (Id: number) => `income/${Id}`,
@@ -20,6 +24,7 @@ export const apiSlice = createApi({
                 method: "POST",
                 body: incomeData,
             }),
+            invalidatesTags: ["Income"],
         }),
         updateIncome: builder.mutation({
             query: (incomeData: updateIncomeDto) => ({
@@ -34,8 +39,18 @@ export const apiSlice = createApi({
                 method: "DELETE",
             }),
         }),
+
         getFixedIncomesByUserId: builder.query({
             query: (userId) => `fixedincome/user/${userId}`,
+            providesTags: ["FixedIncome"],
+        }),
+        createFixedIncome: builder.mutation({
+            query: (fixedIncomeData: createFixedIncomeDto) => ({
+                url: "fixedincome",
+                method: "POST",
+                body: fixedIncomeData,
+            }),
+            invalidatesTags: ["FixedIncome"],
         }),
     }),
 });
@@ -46,5 +61,7 @@ export const {
     useCreateIncomeMutation,
     useUpdateIncomeMutation,
     useDeleteIncomeMutation,
+
     useGetFixedIncomesByUserIdQuery,
+    useCreateFixedIncomeMutation,
 } = apiSlice;
