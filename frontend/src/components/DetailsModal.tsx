64928dfed84df2132f6b1a727ab87e65866d5816
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-13 15:20:20 -0400

    Modals configuration to use modal container and modal fields

--- frontend/src/components/DetailsModal.tsx
+++ frontend/src/components/DetailsModal.tsx
@@ -8,23 +8,30 @@ import {
     useUpdateIncomeMutation,
 } from "../../api/apiSlice";
 import Loader from "./Loader";
-import { modalProps } from "./CreateModal";
+import ModalContainer from "./ModalContainer";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { hideModal } from "../reducers/detailsModalReducers";
 
-function DetailsModal({ show, handleClosing }: modalProps) {
-    const [amount, setAmount] = useState<number>(0);
-    const [details, setDetail] = useState<string>("");
-    const [date, setDate] = useState<Date>(new Date());
-    const [id, setId] = useState<number>(0);
+type detailsModal = {
+    deleteFunction: () => void;
+    updateFunction: () => void;
+    itemId: number;
+    children: React.ReactNode;
+};
 
+function DetailsModal({ children, deleteFunction, updateFunction, itemId }: detailsModal) {
     const { data, error, isLoading } = useGetIncomesByIdQuery(1);
     const [deleteIncome, deleteResult] = useDeleteIncomeMutation();
     const [updateIncome, updateResult] = useUpdateIncomeMutation();
 
-    useEffect(() => {
-        if (!isLoading && data != undefined) {
-            setAmount(data.amount), setDetail(data.details), setDate(data.date), setId(data.id);
-        }
-    }, [isLoading, data]);
+    const show = useAppSelector((state) => state.detailsModal.show);
+    const dispatch = useAppDispatch();
+
+    // useEffect(() => {
+    //     if (!isLoading && data != undefined) {
+    //         setAmount(data.amount), setDetail(data.details), setDate(data.date), setId(data.id);
+    //     }
+    // }, [isLoading, data]);
 
     if (!show) {
         return null;
@@ -48,84 +55,27 @@ function DetailsModal({ show, handleClosing }: modalProps) {
         handleClosing();
     };
 
+    const handleClosing = () => dispatch(hideModal());
+
     return (
-        <div className="absolute left-0 h-[100vh] w-[100vw] bg-black bg-opacity-40 flex">
-            <div className="bg-custom-ly1 m-auto w-[30%] infoContainer1 shadow-[0_0_5px_1px_rgba(0,0,0,0.35)]">
-                {isLoading ? (
-                    <Loader />
-                ) : (
-                    <>
-                        <div className="grid grid-cols-3 w-full">
-                            <p className="col-start-2 m-auto">Income Details</p>
-                            <button
-                                className="ml-auto tableButton flex gap-x-2 p-0 items-center opacity-55 hover:opacity-100"
-                                onClick={handleClosing}
-                            >
-                                <FontAwesomeIcon icon={faXmark} />
-                            </button>
-                        </div>
-                        <form className="flex flex-col gap-y-3 w-full">
-                            <div className="flex flex-col gap-y-1">
-                                <label htmlFor="amount">Amount</label>
-                                <input
-                                    type="number"
-                                    id="amount"
-                                    name="amount"
-                                    className="formInput w-full"
-                                    placeholder="Amount"
-                                    onChange={(e) => setAmount(parseInt(e.target.value))}
-                                    value={amount}
-                                ></input>
-                            </div>
-
-                            <div className="flex flex-col gap-y-1">
-                                <label htmlFor="details">Details</label>
-                                <input
-                                    type="text"
-                                    id="details"
-                                    name="details"
-                                    className="formInput w-full"
-                                    placeholder="Details"
-                                    onChange={(e) => setDetail(e.target.value)}
-                                    value={details}
-                                ></input>
-                            </div>
-
-                            <div className="flex flex-col gap-y-1">
-                                <label htmlFor="date">Date</label>
-                                <input
-                                    type="datetime-local"
-                                    id="date"
-                                    name="date"
-                                    className="formInput w-full"
-                                    placeholder="Date"
-                                    onChange={(e) => setDate(e.target.value)}
-                                    value={date}
-                                ></input>
-                            </div>
-                            <span className="formDivider"></span>
-                            <div className="self-end flex gap-x-2">
-                                <button type="reset" className="formButton" onClick={handleClosing}>
-                                    <p>Cancel</p>
-                                </button>
-                                <button
-                                    className="formButton hover:bg-red-500"
-                                    onClick={(event) => handleDeleteIncome(event)}
-                                >
-                                    <p>Delete</p>
-                                </button>
-                                <button
-                                    className="formButton"
-                                    onClick={(event) => handleUpdateIncome(event)}
-                                >
-                                    <p>Save</p>
-                                </button>
-                            </div>
-                        </form>
-                    </>
-                )}
+        <ModalContainer closingHandler={handleClosing}>
+            {children}
+            <span className="formDivider"></span>
+            <div className="self-end flex gap-x-2">
+                <button type="reset" className="formButton" onClick={handleClosing}>
+                    <p>Cancel</p>
+                </button>
+                <button
+                    className="formButton hover:bg-red-500"
+                    onClick={(event) => handleDeleteIncome(event)}
+                >
+                    <p>Delete</p>
+                </button>
+                <button className="formButton" onClick={(event) => handleUpdateIncome(event)}>
+                    <p>Save</p>
+                </button>
             </div>
-        </div>
+        </ModalContainer>
     );
 }
 
