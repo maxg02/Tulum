4fd05de977e6e7a7447174dcb912f0f3f261f508
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-13 15:19:38 -0400

    Create modal reducers store configurations and rootState and AppDispatch types exporting

--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice.ts";
 import detailsModalReducer from "../src/reducers/detailsModalReducers";
+import createModalReducer from "../src/reducers/createModalReducers";
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         detailsModal: detailsModalReducer,
+        createModal: createModalReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
