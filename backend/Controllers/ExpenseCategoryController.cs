0b1459e8acfeac0a3597d32b3de99aab285fe279
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-22 23:22:29 -0400

    Expense category get by user id controllers

---
+++ backend/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,38 @@
+﻿using backend.Mappers;
+using backend.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseCategoryController : ControllerBase
+    {
+        private readonly IExpenseCategoryRepo _expenseCategoryRepo;
+
+        public ExpenseCategoryController(IExpenseCategoryRepo expenseCategoryRepo)
+        {
+            _expenseCategoryRepo = expenseCategoryRepo;
+        }
+
+        [HttpGet("user/full/{userId}")]
+        public async Task<IActionResult> GetUserExpenseCategoryFull(int userId)
+        {
+            var expenseCategories = await _expenseCategoryRepo.GetByUserIdFullAsync(userId);
+
+            var expenseCategoriesFullDto = expenseCategories.Select(ec => ec.ToExpenseCategoryFullDto());
+
+            return Ok(expenseCategoriesFullDto);
+        }
+
+        [HttpGet("user/budget/{userId}")]
+        public async Task<IActionResult> GetUserExpenseCategoryWithBudget(int userId)
+        {
+            var expenseCategories = await _expenseCategoryRepo.GetByUserIdFullAsync(userId);
+
+            var expenseCategoriesFullDto = expenseCategories.Select(ec => ec.ToExpenseCategoryWithBudgetDto());
+
+            return Ok(expenseCategoriesFullDto);
+        }
+    }
+}
