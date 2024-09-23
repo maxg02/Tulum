cdb72af76bf5fbe57952a469cc811acf24182b13
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-22 23:22:18 -0400

    Expense Category Mappers

---
+++ backend/Mappers/ExpenseCategoryMappers.cs
@@ -0,0 +1,37 @@
+﻿using backend.Dtos.ExpenseCategory;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class ExpenseCategoryMappers
+    {
+        public static ExpenseCategoryFullDto ToExpenseCategoryFullDto(this ExpenseCategory ExpenseCategoryModel)
+        {
+            return new ExpenseCategoryFullDto
+            {
+                Id = ExpenseCategoryModel.Id,
+                Category = ExpenseCategoryModel.Category,
+                Expenses = ExpenseCategoryModel.Expenses,
+                FixedExpenses = ExpenseCategoryModel.FixedExpenses,
+                BudgetPlan = ExpenseCategoryModel.BudgetPlan,
+            };
+        }
+        public static ExpenseCategoryWithBudgetDto ToExpenseCategoryWithBudgetDto(this ExpenseCategory ExpenseCategoryModel)
+        {
+            return new ExpenseCategoryWithBudgetDto
+            {
+                Id = ExpenseCategoryModel.Id,
+                Category = ExpenseCategoryModel.Category,
+                BudgetPlan = ExpenseCategoryModel.BudgetPlan,
+            };
+        }
+        public static ExpenseCategory ToExpenseCategoryFromCreateDto(this CreateExpenseCategoryRequestDto ExpenseCategoryDto)
+        {
+            return new ExpenseCategory
+            {
+                Category = ExpenseCategoryDto.Category,
+                UserId = 1
+            };
+        }
+    }
+}
