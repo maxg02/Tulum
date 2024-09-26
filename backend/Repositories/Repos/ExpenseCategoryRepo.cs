48b61fc61545b74ac10530baf03c53dc6cc78497
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-26 09:16:46 -0400

    Expense Category repo update, delete, create and get by id

--- backend/Repositories/Repos/ExpenseCategoryRepo.cs
+++ backend/Repositories/Repos/ExpenseCategoryRepo.cs
@@ -14,44 +14,69 @@ namespace backend.Repositories.Repos
         
         public async Task<List<ExpenseCategory>> GetByUserIdFullAsync(int userId)
         {
-            var ExpenseCategories = await _context.ExpenseCategories
+            var expenseCategories = await _context.ExpenseCategories
                 .Where(ec => ec.UserId == userId)
                 .Include(ec => ec.Expenses)
                 .Include(ec => ec.FixedExpenses)
                 .Include(ec => ec.BudgetPlan)
                 .ToListAsync();
 
-            return ExpenseCategories;
+            return expenseCategories;
         }
 
         public async Task<List<ExpenseCategory>> GetByUserIdWithBudgetAsync(int userId)
         {
-            var ExpenseCategories = await _context.ExpenseCategories
+            var expenseCategories = await _context.ExpenseCategories
                 .Where(ec => ec.UserId == userId)
                 .Include(ec => ec.BudgetPlan)
                 .ToListAsync();
 
-            return ExpenseCategories;
+            return expenseCategories;
         }
 
-        public Task<ExpenseCategory?> GetByIdAsync(int id)
+        public async Task<ExpenseCategory?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var expenseCategory = await _context.ExpenseCategories.FirstOrDefaultAsync(ec => ec.Id == id);
+
+            return expenseCategory;
         }
 
-        public Task<ExpenseCategory> CreateAsync(ExpenseCategory expenseCategory)
+        public async Task<ExpenseCategory> CreateAsync(ExpenseCategory expenseCategory)
         {
-            throw new NotImplementedException();
+            await _context.ExpenseCategories.AddAsync(expenseCategory);
+            await _context.SaveChangesAsync();
+
+            return expenseCategory;
         }
 
-        public Task<ExpenseCategory?> DeleteAsync(int id)
+        public async Task<ExpenseCategory?> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var expenseCategory = await GetByIdAsync(id);
+
+            if (expenseCategory == null)
+            {
+                return null;
+            }
+
+            _context.ExpenseCategories.Remove(expenseCategory);
+            await _context.SaveChangesAsync();
+
+            return expenseCategory;
         }
 
-        public Task<ExpenseCategory?> UpdateAsync(int id, UpdateExpenseCategoryRequestDto expenseCategoryDto)
+        public async Task<ExpenseCategory?> UpdateAsync(int id, UpdateExpenseCategoryRequestDto expenseCategoryDto)
         {
-            throw new NotImplementedException();
+            var expenseCategory = await GetByIdAsync(id);
+
+            if (expenseCategory == null)
+            {
+                return null;
+            }
+
+            expenseCategory.Category = expenseCategoryDto.Category;
+            await _context.SaveChangesAsync();
+
+            return expenseCategory;
         }
     }
 }
