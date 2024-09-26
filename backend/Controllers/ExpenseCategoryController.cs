af4b7fb4fa0103f52e1b0534e1436a55e69d3eca
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-09-26 09:17:29 -0400

    Expense category create, update, delete, and get by id endpoints creation

--- backend/Controllers/ExpenseCategoryController.cs
+++ backend/Controllers/ExpenseCategoryController.cs
@@ -1,4 +1,5 @@
-﻿using backend.Mappers;
+﻿using backend.Dtos.ExpenseCategory;
+using backend.Mappers;
 using backend.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,7 @@ namespace backend.Controllers
         }
 
         [HttpGet("user/full/{userId}")]
-        public async Task<IActionResult> GetUserExpenseCategoryFull(int userId)
+        public async Task<IActionResult> GetUserExpenseCategoryFull([FromRoute] int userId)
         {
             var expenseCategories = await _expenseCategoryRepo.GetByUserIdFullAsync(userId);
 
@@ -26,13 +27,60 @@ namespace backend.Controllers
         }
 
         [HttpGet("user/budget/{userId}")]
-        public async Task<IActionResult> GetUserExpenseCategoryWithBudget(int userId)
+        public async Task<IActionResult> GetUserExpenseCategoryWithBudget([FromRoute] int userId)
         {
             var expenseCategories = await _expenseCategoryRepo.GetByUserIdFullAsync(userId);
 
             var expenseCategoriesFullDto = expenseCategories.Select(ec => ec.ToExpenseCategoryWithBudgetDto());
 
             return Ok(expenseCategoriesFullDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExpenseCategory([FromRoute] int id)
+        {
+            var expenseCategory = await _expenseCategoryRepo.GetByIdAsync(id);
+
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(expenseCategory);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExpenseCategory([FromBody] CreateExpenseCategoryRequestDto expenseCategoryDto)
+        {
+            var expenseCategory = await _expenseCategoryRepo.CreateAsync(expenseCategoryDto.ToExpenseCategoryFromCreateDto());
+
+            return CreatedAtAction(nameof(GetExpenseCategory), new { id = expenseCategory.Id }, expenseCategory.ToExpenseCategoryFullDto());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExpenseCategory([FromRoute] int id, [FromBody] UpdateExpenseCategoryRequestDto expenseCategoryDto)
+        {
+            var expenseCategory = await _expenseCategoryRepo.UpdateAsync(id, expenseCategoryDto);
+
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(expenseCategory.ToExpenseCategoryFullDto());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpenseCategory([FromRoute] int id)
+        {
+            var expenseCategory = await _expenseCategoryRepo.DeleteAsync(id);
+
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
