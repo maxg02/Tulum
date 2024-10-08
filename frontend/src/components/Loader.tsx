c9be81e3fd344377165e7cc1fe3334201b2b5622
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-10-08 16:24:35 -0400

    Loader creation

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Loader() {
-    return <div className="w-full h-full">Loading...</div>;
+    return (
+        <div className="w-full h-full flex">
+            <div className="my-auto mx-auto h-12 rounded-full border-8 border-custom-accent/40 border-t-custom-accent aspect-square animate-spin"></div>
+        </div>
+    );
 }
 
 export default Loader;
