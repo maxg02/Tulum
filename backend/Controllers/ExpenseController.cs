648f79ce8ce42e4fd4cd68859fffed511bd3b94e
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-10-11 15:08:31 -0400

    Creation of expense controller and create expense endpoint

---
+++ backend/Controllers/ExpenseController.cs
@@ -0,0 +1,26 @@
+﻿using backend.Data;
+using backend.Dtos.Expense;
+using backend.Mappers;
+using backend.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly IExpenseRepo _expenseRepo;
+
+        public ExpenseController(IExpenseRepo expenseRepo) => _expenseRepo = expenseRepo;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExpense(CreateExpenseRequestDto expenseDto)
+        {
+            var expense = await _expenseRepo.CreateAsync(expenseDto.toExpenseFromCreateDto());
+
+            return Ok(expense);
+        }
+    }
+}
