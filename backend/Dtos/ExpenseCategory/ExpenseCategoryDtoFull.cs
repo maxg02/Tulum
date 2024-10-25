cc1f811c2e19439aa87434b38e7d2dc18bb50445
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-10-25 16:32:36 -0400

    Fixed expense model import fixing on expense category dto

--- backend/Dtos/ExpenseCategory/ExpenseCategoryDtoFull.cs
+++ backend/Dtos/ExpenseCategory/ExpenseCategoryDtoFull.cs
@@ -7,7 +7,7 @@ namespace backend.Dtos.ExpenseCategory
         public int Id { get; set; }
         public string Category { get; set; } = String.Empty;
         public List<Models.Expense> Expenses { get; set; } = new List<Models.Expense>();
-        public List<FixedExpense> FixedExpenses { get; set; } = new List<FixedExpense>();
+        public List<Models.FixedExpense> FixedExpenses { get; set; } = new List<Models.FixedExpense>();
         public Models.BudgetPlan? BudgetPlan { get; set; }
     }
 }
