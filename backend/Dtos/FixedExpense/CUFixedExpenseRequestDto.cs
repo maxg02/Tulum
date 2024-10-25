e60013445b4d346e5e955fefc0646eae4a7135ac
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-10-25 16:31:02 -0400

    Fixed Expense Dtos and mappers creation

---
+++ backend/Dtos/FixedExpense/CUFixedExpenseRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Dtos.FixedExpense
+{
+    public class CUFixedExpenseRequestDto
+    {
+        public int Amount { get; set; }
+        public string Details { get; set; } = String.Empty;
+        public int Periodicity { get; set; }
+        public int ExpenseCategoryId { get; set; }
+    }
+}
---
+++ backend/Mappers/FixedExpenseMappers.cs
@@ -0,0 +1,19 @@
+﻿using backend.Dtos.FixedExpense;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class FixedExpenseMappers
+    {
+        public static FixedExpense toFixedExpenseFromCreateDto(this CUFixedExpenseRequestDto fixedExpenseDto)
+        {
+            return new FixedExpense
+            {
+                Amount = fixedExpenseDto.Amount,
+                Periodicity = fixedExpenseDto.Periodicity,
+                Details = fixedExpenseDto.Details,
+                ExpenseCategoryId = fixedExpenseDto.ExpenseCategoryId,
+            };
+        }
+    }
+}
