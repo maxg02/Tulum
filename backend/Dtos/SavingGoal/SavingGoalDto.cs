d88b85ee8526984e226ab6732fd9e18e9c6e0977
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-10-31 11:51:51 -0400

    saving goal navigation property for goal contribution fixing

--- backend/Dtos/SavingGoal/SavingGoalDto.cs
+++ backend/Dtos/SavingGoal/SavingGoalDto.cs
@@ -9,6 +9,6 @@ namespace backend.Dtos.SavingGoal
         public int Goal { get; set; }
         public int? FixedContribution { get; set; }
         public int? Periodicity { get; set; }
-        public List<GoalContribution> GoalContributions { get; set; } = new List<GoalContribution>();
+        public List<Models.GoalContribution> GoalContributions { get; set; } = new List<Models.GoalContribution>();
     }
 }
