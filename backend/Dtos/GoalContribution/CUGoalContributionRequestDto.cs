216f68f509aee5c4ed644b5f3e50b22e8c7b4ac0
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-10-31 11:45:05 -0400

    Goal Contributions dtos and mappers creation

---
+++ backend/Dtos/GoalContribution/CUGoalContributionRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Dtos.GoalContribution
+{
+    public class CUGoalContributionRequestDto
+    {
+        public int Amount { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public int SavingGoalId { get; set; }
+    }
+}
