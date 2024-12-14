37fad6b16616f9158ee8e0faf669f96376ff4736
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-14 12:18:30 -0400

    users get views creation

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from users.models import AppUser
+from users.serializers import UserSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def get_users(request):
+    users = AppUser.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_user(request, pk):
+    user = AppUser.objects.get(id=pk)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
