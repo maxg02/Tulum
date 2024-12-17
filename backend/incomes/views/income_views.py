1200f35c97dd908ffc4fcefed606590592c2521c
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-17 10:43:28 -0400

    Changing auth verify to permission classes

--- backend/incomes/views/income_views.py
+++ backend/incomes/views/income_views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from incomes.models import Income
 from incomes.serializers import IncomeSerializer
 
 
 class IncomeList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    @method_decorator(login_required)
     def get(self, request):
         user = request.user
         incomes = Income.objects.filter(user=user)
         serializer = IncomeSerializer(incomes, many=True)
         return Response(serializer.data)
 
-    @method_decorator(login_required)
     def post(self):
         user = self.request.user
         serializer = IncomeSerializer(data=self.request.data)
@@ -29,15 +27,14 @@ class IncomeList(APIView):
 
 
 class IncomeDetails(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    @method_decorator(login_required)
     def get_object(self, pk):
         try:
             return Income.objects.get(id=pk)
         except Income.DoesNotExist:
             Response("Income doesn't exists", status=status.HTTP_404_NOT_FOUND)
 
-    @method_decorator(login_required)
     def put(self, request, pk):
         income = self.get_object(pk)
         serializer = IncomeSerializer(income, data=request.data)
@@ -47,7 +44,6 @@ class IncomeDetails(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @method_decorator(login_required)
     def delete(self, request, pk):
         income = self.get_object(pk)
         income.delete()
