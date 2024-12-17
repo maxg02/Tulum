dd507f9a5665261e3855af1acf068b9ef128af20
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-17 10:43:01 -0400

    JWT authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,6 +85,14 @@ DATABASES = {
     }
 }
 
+# Rest Framework Auth
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('incomes/', include('incomes.urls.income_urls'))
+    path('incomes/', include('incomes.urls.income_urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
