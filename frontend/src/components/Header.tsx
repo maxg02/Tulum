66dbf1750fb8acee18ab36cd77770badf981338e
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-22 21:17:32 -0400

    Log out button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { SectionType } from "./Sidebar";
+import { useAppDispatch } from "../hooks";
+import { logOut } from "../reducers/userReducers";
 
 export default function Header({ currentSection }: { currentSection: SectionType }) {
+    const dispatch = useAppDispatch();
+
     return (
         <div className="flex items-center justify-between h-14">
             <h1 className="text-5xl">{currentSection}</h1>
@@ -9,6 +13,7 @@ export default function Header({ currentSection }: { currentSection: SectionType
                 <img className="h-full w-auto rounded-xl" src="/profilePic.jpeg" alt="Profile Pic" />
                 <p>Maria Ambroise</p>
             </div>
+            <button onClick={() => dispatch(logOut())}>Logout</button>
         </div>
     );
 }
