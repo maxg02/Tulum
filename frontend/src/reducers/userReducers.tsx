fc709bdf2dbf1257481e818bac520e0d4dbb0242
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-22 21:07:49 -0400

    Creating setUserInfo, refreshUserToken and LogOut reducers

--- frontend/src/reducers/userReducers.tsx
+++ frontend/src/reducers/userReducers.tsx
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { decodeToken } from "react-jwt";
 
 export type userInfo = {
     userInfo: {
@@ -16,13 +17,39 @@ export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-        setUserInfo: (state, action: PayloadAction<userInfo>) => {
-            state.tokens = action.payload.tokens;
-            state.userInfo = action.payload.userInfo;
+        setUserInfo: (state, action: PayloadAction<{ access: string; refresh: string }>) => {
+            const tokens = action.payload;
+            const tokenInfo = decodeToken(tokens.access);
+
+            const userInfo: userInfo = {
+                userInfo: {
+                    fullName: tokenInfo.full_name,
+                },
+                tokens: {
+                    access: tokens.access,
+                    refresh: tokens.refresh,
+                },
+            };
+            localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+            state.tokens = userInfo.tokens;
+            state.userInfo = userInfo.userInfo;
+        },
+        refreshUserToken: (state, action: PayloadAction<{ access: string }>) => {
+            state.tokens!.access = action.payload.access;
+            const userInStorage = JSON.parse(localStorage.getItem("userInfo")!);
+            userInStorage.tokens.access = action.payload.access;
+            localStorage.setItem("userInfo", JSON.stringify(userInStorage));
+        },
+        logOut: (state) => {
+            debugger;
+            state.tokens = null;
+            state.userInfo = null;
+            localStorage.removeItem("userInfo");
         },
     },
 });
 
-export const { setUserInfo } = userSlice.actions;
+export const { setUserInfo, logOut, refreshUserToken } = userSlice.actions;
 
 export default userSlice.reducer;
