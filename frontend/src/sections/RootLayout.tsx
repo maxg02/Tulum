b3e4b46ecc947cc5bdd3dedb8e8a541f6f47809c
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-22 21:16:56 -0400

    log in state validation on root layout

--- frontend/src/sections/RootLayout.tsx
+++ frontend/src/sections/RootLayout.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { setUserInfo } from "../reducers/userReducers";
 
 export default function RootLayout() {
+    const dispatch = useAppDispatch();
+    //check if there's user on local storage
+    const userStorage = localStorage.getItem("userInfo");
+    //check if there's user on state
+    const userToken = useAppSelector((state) => state.user.tokens);
+
+    if (!userStorage) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (!userToken) {
+        const userJson = JSON.parse(userStorage);
+        dispatch(setUserInfo(userJson.tokens));
+    }
+
     return (
         <div className="bg-custom-bg flex w-screen h-screen px-8">
             <Outlet />
