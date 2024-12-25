70c3c24b2d3f44a9e7e818d59c696fb8d8cd67b6
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2024-12-25 11:14:25 -0400

    Fixed income serializer

--- backend/incomes/serializers.py
+++ backend/incomes/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from incomes.models import Income
+from incomes.models import Income, FixedIncome
 
 
 class IncomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Income
+        exclude = ["user"]
+
+
+class FixedIncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FixedIncome
         exclude = ["user"]
