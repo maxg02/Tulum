ef8e6e4244b238c5208c169661e2dc218d604719
Maxg02 <maxdanielsanchezgarcia@gmail.com>
2025-08-11 12:41:56 -0400

    Creation of error messages component and implementation on register

--- backend/Dtos/Auth/UserRegisterRequestDto.cs
+++ backend/Dtos/Auth/UserRegisterRequestDto.cs
@@ -7,14 +7,13 @@ namespace backend.Dtos.Auth
 {
     public class UserRegisterRequestDto
     {
-        [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
         [Required]
         public string Name { get; set; } = string.Empty;
-        [Required(ErrorMessage = "The Password field is required")]
-        [MinLength(8, ErrorMessage = "Password must have 8 characters")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$", ErrorMessage = "The password must include a lowercase letter, an uppercase letter and a digit")]
+        [Required(ErrorMessage = "The Password field is required.")]
+        [MinLength(8, ErrorMessage = "Password must have 8 characters.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$", ErrorMessage = "The password must include a lowercase letter, an uppercase letter and a digit.")]
         public string Password { get; set; } = string.Empty;
 
     }
---
+++ frontend/src/components/Misc/ErrorMessage.tsx
@@ -0,0 +1,36 @@
+import { AlertCircleIcon, Cancel01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+
+function Error({ error }: { error: string }) {
+    return (
+        <div className="mb-2 bg-white border-l-4 border-red-500 shadow-lg z-50 rounded-md flex items-center">
+            <div className="p-2">
+                <HugeiconsIcon size={30} className="text-red-500" icon={AlertCircleIcon} />
+            </div>
+            <span className="text-red-400 p-1 ps-0">{error}</span>
+            <div
+                className="p-2 ms-auto cursor-pointer text-gray-400 hover:text-gray-500"
+                onClick={(e) => e.currentTarget.parentElement?.remove()}
+            >
+                <HugeiconsIcon size={15} strokeWidth={2} icon={Cancel01Icon} />
+            </div>
+        </div>
+    );
+}
+
+function ErrorMessage({ error }: { error: string[] }) {
+    const Errors = () =>
+        error.map((err, index) => (
+            <div key={index}>
+                <Error error={err} />
+            </div>
+        ));
+
+    return (
+        <div className="fixed top-0 right-0 p-4 w-full md:w-[30rem]">
+            <Errors />
+        </div>
+    );
+}
+
+export default ErrorMessage;
