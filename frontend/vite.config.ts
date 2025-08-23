657ca517cd635cab15d4375902db39aac8b535bd
Maxg02 <maxdanielsanchezgarcia@gmail.com>
2025-08-23 12:24:38 -0400

    Config vite for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,21 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
-});
+    base: mode === "production" ? "/ExpControlFront/" : "/",
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id.toString().split("node_modules/")[1].split("/")[0].toString();
+                    }
+                },
+            },
+        },
+    },
+}));
